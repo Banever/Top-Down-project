7616bae4b58c10b470acd84def429039f06df01e
Vanever <ivanbarrioscabarcas@gmail.com>
2023-03-09 21:43:27 -0500

    Se implemento una pequeña barra de inventario del personaje y un item usable.No pude implementar un cofre al momento ya que no comprendo bien como funciona, solo cree un objeto que suelta items si se colisiona con el.

---
+++ Assets/Scripts/Corazon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corazon : MonoBehaviour
+{
+      public void Usar()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventario Inventario;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        Inventario = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventario>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < Inventario.Slots.Length; i++)
+            {
+                if (Inventario.isFull[i] == false)
+                {
+                    Inventario.isFull[i] = true;
+                    Instantiate(itemButton, Inventario.Slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;                   
+                }
+            }
+        }
+    }
+}
